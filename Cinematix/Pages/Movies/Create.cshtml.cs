2354788bf0101e098ffeaed4217b5a10abe387a6
dominiquetoonen <54108097+dominiquetoonen@users.noreply.github.com>
2020-09-08 13:29:52 +0200

    Add files via upload

---
+++ Cinematix/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Cinematix.Data;
+using Cinematix.Models;
+
+namespace Cinematix
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Cinematix.Data.CinematixContext _context;
+
+        public CreateModel(Cinematix.Data.CinematixContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movies.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
